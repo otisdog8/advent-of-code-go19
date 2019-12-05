ba8261bd26ec6d2443f3f38547553d7d377309bd
Otis Root <otis@otisroot.com>
2019-12-04 20:53:41 -0800

    Problem 1 done

---
+++ Problem 1/answers.go
@@ -0,0 +1,51 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"log"
+	"math"
+	"strconv"
+	"strings"
+)
+
+func main() {
+	data, err := ioutil.ReadFile("inputs.txt")
+	sum := 0
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	dataparts := strings.Split(string(data), "\n")
+
+	for i := 0; i < len(dataparts); i++ {
+		num, err := strconv.Atoi(dataparts[i])
+		if err != nil {
+			log.Fatal(err)
+		}
+		sum += int(math.Floor(float64(num)/3)) - 2
+		fuel := int(math.Floor(float64(num)/3)) - 2
+
+		for int(math.Floor(float64(fuel)/3))-2 > 0 {
+
+			sum += int(math.Floor(float64(fuel)/3)) - 2
+			fuel = int(math.Floor(float64(fuel)/3)) - 2
+
+		}
+	}
+
+	fmt.Println(strconv.Itoa(sum))
+
+	fuel := sum
+
+	for int(math.Floor(float64(fuel)/3))-2 > 0 {
+
+		sum += int(math.Floor(float64(fuel)/3)) - 2
+		fuel = int(math.Floor(float64(fuel)/3)) - 2
+
+	}
+
+	fmt.Println(strconv.Itoa(sum))
+
+}
